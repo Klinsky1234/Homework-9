3d425c7f2d18c002135b3b386208f3977d3486df
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2023-01-17 18:43:38 +0300

    64

--- Program.cs
+++ Program.cs
@@ -4,3 +4,18 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
 
+int N;
+Console.Write($"enter N: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+PrintNumbers(N, 1);
+
+void PrintNumbers(int start, int n)
+{
+    if (start < n)
+        return;
+    Console.Write($"{start} ");
+    PrintNumbers(start - 1, n);
+}
+
+
